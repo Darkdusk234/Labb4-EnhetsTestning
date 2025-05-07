9c960bf942d247c3fa41ac4c021be3a3496eb5ee
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-07 11:23:25 +0200

    Created test for BorrowBook checking if borrowed book is marked as borrowed

--- Labb4-EnhetsTesting-Test/LibrarySystemTests.cs
+++ Labb4-EnhetsTesting-Test/LibrarySystemTests.cs
@@ -252,4 +252,15 @@ public class LibrarySystemTests
 
         Assert.IsFalse(actual);
     }
+
+    [TestMethod]
+    public void BorrowBook_ExistingISBNNumberInputtedCheckIfBookIsMarkedAsBorrowed_True()
+    {
+        var library = new LibrarySystem();
+
+        library.BorrowBook("9780451524935");
+        var actual = library.SearchByISBN("9780451524935");
+
+        Assert.IsTrue(actual.IsBorrowed);
+    }
 }
